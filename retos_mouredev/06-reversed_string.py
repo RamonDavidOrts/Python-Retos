4d91889b88fc20c137f229250fdb03776ea86f7d
Ramón David Orts <51417237+RamonDavidOrts@users.noreply.github.com>
2022-09-24 18:00:42 +0200

    Update 06-reversed_string.py

--- retos_mouredev/06-reversed_string.py
+++ retos_mouredev/06-reversed_string.py
@@ -5,12 +5,11 @@ def reverse(text):
     return new_text
 
 def recursive_reverse(text, reversed_text="", index=None):
-    if index == -1:
-        return reversed_text
-    elif index is None:
+    if index == 0:
+        return text[0]
+    if index == None:
         index = len(text) - 1
-    reversed_text += text[index]
-    return recursive_reverse(text, reversed_text, index - 1)
+    return text[index] + recursive_reverse(text, index - 1)
 
 text = "hocus pocus"
 print(reverse(text))
