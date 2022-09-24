25864ec71be43d28f47691450b38aa7457517df1
Ramón David Orts <51417237+RamonDavidOrts@users.noreply.github.com>
2022-09-24 18:28:38 +0200

    Update 08-decimal_to_binary.py

--- retos_mouredev/08-decimal_to_binary.py
+++ retos_mouredev/08-decimal_to_binary.py
@@ -12,16 +12,14 @@ def dec2bin(dec):
 
 def recursive_dec2bin(dec, newdec=None, bin=""):
     if dec == 0:
-        return 0
-    if newdec is None:
-        newdec = abs(dec)
-    elif newdec < 1:
+        return "0"
+    if newdec == 1:
         if dec < 0:
-            bin = "-" + bin
-        return bin
-    bin = str(newdec % 2) + bin
-    newdec = newdec // 2
-    return recursive_dec2bin(dec, newdec, bin)
+            return "-1"
+        return "1"
+    if newdec == None:
+        newdec = abs(dec)
+    return recursive_dec2bin(dec, newdec // 2) + str(newdec % 2)
 
 print(dec2bin(45678313))
 print(recursive_dec2bin(45678313))
